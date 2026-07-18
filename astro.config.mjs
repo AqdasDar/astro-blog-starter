f1ab86597699832ca86b50bf3f65e38654527257
Aqdas <Aqdasdar91@gmail.com>
2026-07-18 02:02:01 +0200

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // C'est LA ligne vitale pour que le Footer et le BaseHead ne plantent pas
   site: 'https://aesthetic-production.fr', 
   
   output: 'static',
+  integrations: [sitemap()],
 });
